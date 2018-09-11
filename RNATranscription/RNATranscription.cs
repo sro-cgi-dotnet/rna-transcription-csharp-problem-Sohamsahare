5cac081e4caba58f59b1cb463fdba783cb28866a
Soham Sahare <sohamsahare@gmail.com>
2018-09-11 14:30:33 +0530

    first attempt which passes all test cases

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -4,9 +4,29 @@ namespace RNATranscriptionModule
 {
     public static class RnaTranscription
     {
-        public static string ToRna(string nucleotide)
+        public static string ToRna(string nucleotides)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string rnaString = "";
+            foreach(char nucleotide in nucleotides){
+                switch(nucleotide){
+                    case 'G':
+                            rnaString += "C";
+                            break;
+                    case 'C':
+                            rnaString += "G";
+                            break;
+                    case 'T':
+                            rnaString += "A";
+                            break;
+                    case 'A':
+                            rnaString += "U";
+                            break;
+                    default:
+                            throw new ArgumentException("Nucleotides can only be G, C, T and A");
+                            break;
+                }
+            }
+            return rnaString;
         }
     }
 }
